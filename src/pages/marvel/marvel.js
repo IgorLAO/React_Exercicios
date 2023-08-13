b31dad8869c7c8f6986d0525d0a64a14c4b41a80
igorLAO <igooli670@gmail.com>
2023-08-13 16:38:16 -0300

    marvel final

--- src/pages/marvel/marvel.js
+++ src/pages/marvel/marvel.js
@@ -12,42 +12,66 @@ export default function Marvel() {
     const [extension, setExtension] = useState('')
     const [ImageUrl, setImageUrl] = useState([])
     const [Search, setSearch] = useState('p')
+    const [offset, setOffset] = useState('offset=0')
+    const [offsetNum, setOffsetNum] = useState(0)
+    const [Limit, setLimit] = useState('limit=20')
+    const [LimitNum, setLimitNum] = useState(20)
+    const [und, setUnd] = useState('')
 
-    if(Search === ''){
+    if (Search === '') {
 
         setSearch('l')
     }
 
-    useEffect(() =>{
+    useEffect(() => {
 
-        const FetchData = async () =>{
-            let response = await fetch(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=d29133ecb4f4ad60054a59dda5ba8ae0&hash=b63257d6639546f32f66b9f03a0138ba&nameStartsWith=${Search}&offset=20&limit=20`)
+        const FetchData = async () => {
+            let response = await fetch(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=d29133ecb4f4ad60054a59dda5ba8ae0&hash=b63257d6639546f32f66b9f03a0138ba&nameStartsWith=${Search}&${offset}&${Limit}`)
             let data = await response.json()
 
-          setList(data.data.results)
-          console.log(data.data.results)
-          let urls = []
-          setImageUrl(urls)
-          ImageUrl.push(urls)
-          
-          for(let i = 0; i < data.data.results.length; i++ ){
+            setList(data.data.results)
+            console.log(data.data.results)
+            let urls = []
+            setImageUrl(urls)
+            ImageUrl.push(urls)
+
+            for (let i = 0; i < data.data.results.length; i++) {
 
                 const character = data.data.results[i]
 
-                if(character.thumbnail && character.thumbnail.path && character.thumbnail.extension){
-                    let imgurl = character.thumbnail.path 
-                    let extension = character.thumbnail.extension 
-                    urls.push(imgurl + '.' +extension)
-
+                
+                if (character.thumbnail != undefined && character.thumbnail.path != undefined && character.thumbnail.extension != undefined ) {
+                    let imgurl = character.thumbnail.path
+                    let extension = character.thumbnail.extension
+                    urls.push(imgurl + '.' + extension)
                 }
             }
-            
-      }
-      FetchData()
+
+        }
+        FetchData()
 
 
-    }, [Search])
+    }, [Search, offset, Limit])
 
+    const next = () => {
+        const num = offsetNum + 20
+        setOffsetNum(num)
+        setOffset(`offset=${num}`)
+    }
+
+    const previous = () => {
+        const num = offsetNum - 20
+        setOffsetNum(num)
+        setOffset(`offset=${num}`)
+    }
+
+    const Mais = () => {
+        const num  = LimitNum + 20
+
+        setLimitNum(num)
+
+        setLimit(`limit=${num}`)
+    }
 
 
 
@@ -71,35 +95,41 @@ export default function Marvel() {
 
             <div className="content">
                 <div className="texts">
-                        <h1>Personagens da MARVEL</h1>
+                    <div className="textsConfig">
+
+                    <h1>Personagens da MARVEL</h1>
                     <div className="desc">
                         <p>Lorem ipsum dolor sit amet, consectetura adipiscing elit. Ut elementum augue ut ligula malesuada blandit. Quisque tempor ex quis congue malesuada. Pellentesque est eros, aliquam non malesuada et, molestie ut purus.</p>
                         <div className="search-bar">
-                            <img src={lupa} alt=""  />
+                            <img src={lupa} alt="" />
                             <input type="text" placeholder="Nome do personagem" onChange={e => setSearch(e.target.value)} />
                         </div>
+                <div className="btnQuery" >
+                    <button  style={{padding: 10, borderRadius: 40, border: 'none', margin: 10, height: 50, width: 50 }} onClick={Mais}> mais... </button>
+                    <button style={{ padding: 10, borderRadius: 40, border: 'none', margin: 10 }} onClick={previous}> pagina anterior </button>
+                    <button style={{ padding: 10, borderRadius: 40, border: 'none', margin: 10 }} onClick={next}> prox pagina </button>
+                </div>
                     </div>
-                </div> 
-                <div  >
-                    <button style={{padding: 10, borderRadius: 40, border: 'none', margin: 10}}> pagina anterior </button>
-                    <button style={{padding: 10, borderRadius: 40, border: 'none', margin: 10}} > prox pagina </button>
+                    </div>
+                </div>
+                <div>
 
                 </div>
 
                 <div className="personagens">
-                 
-                    {list.map((item, index) => 
-                    <div key={index} className="card" >
-                        <img src={ImageUrl[index]} alt=""/>  
+
+                    {list.map((item, index) =>
+                        <div key={index} className="card" >
+                            <img src={ImageUrl[index]} alt="" />
                             <div className="descName">
-                            <h3>{item.name}</h3>
+                                <h3>{item.name}</h3>
                                 <h5>
-                                    aaaaaaaaaaaaaaaaaaaaaaa <br/> aaaaaaaaaaaaaaaaaaaaaaa 
+                                        {item.description}                               
                                 </h5>
-                                
+
                             </div>
-                    </div>
-                            )}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
